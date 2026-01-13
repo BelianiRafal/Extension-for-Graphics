c4a496032b5ce9016a19c93028a024efa88c121e
kkazzann <kamil.kazaniecki@beliani.net>
2026-01-13 14:15:43 +0100

    fix: add host permission & 'scripting' permission to manifest

--- wxt.config.ts
+++ wxt.config.ts
@@ -4,8 +4,18 @@ import { defineConfig } from 'wxt';
 export default defineConfig({
   srcDir: 'src',
   modules: ['@wxt-dev/module-react'],
-    manifest: {
-    permissions: ['tabs'],
+  manifest: {
+    permissions: ['tabs', 'scripting'],
+    host_permissions: [
+      'https://prolodev.prologistics.info/shop_banners.php*',
+      'https://www.prologistics.info/shop_banners.php*',
+      'https://prolodev.prologistics.info/shop_banner.php*',
+      'https://www.prologistics.info/shop_banner.php*',
+      'https://prolodev.prologistics.info/',
+      'https://prolodev.prologistics.info/start.php',
+      'https://prolodev.prologistics.info/timestamp_filter.php',
+      'http://localhost/*',
+    ],
   },
   webExt: {
     openDevtools: true,
